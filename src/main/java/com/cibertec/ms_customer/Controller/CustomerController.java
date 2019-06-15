5d7d955195dc24c3bf9648f8fbe4b67d2fee0a5d
JOAN_PC <jorge.larosa@vfcons.com>
2019-06-15 11:25:00 -0500

    formato

--- src/main/java/com/cibertec/ms_customer/Controller/CustomerController.java
+++ src/main/java/com/cibertec/ms_customer/Controller/CustomerController.java
@@ -6,36 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 public class CustomerController {
 
+    @Autowired
+    private CustomerService customerService;
 
-   @Autowired
-   private CustomerService customerService;
+    @GetMapping(path = "/customer")
+    public ResponseEntity<Object> retriveAll() {
+        return customerService.getAll();
+    }
 
-   @GetMapping(path = "/customer")
-   public ResponseEntity<Object> retriveAll() {
-      return customerService.getAll();
-   }
+    @GetMapping(path = "/customer/{id}")
+    public ResponseEntity<Object> retriveOne(@PathVariable Integer id) {
+        return customerService.getOne(id);
+    }
 
-   @GetMapping(path = "/customer/{id}")
-   public ResponseEntity<Object> retriveOne(@PathVariable Integer id) {
-      return customerService.getOne(id);
-   }
+    @PostMapping(path = "/customer")
+    public ResponseEntity<Object> create(@RequestBody Customer customer) {
+        return customerService.insert(customer);
+    }
 
-   @PostMapping(path = "/customer")
-   public ResponseEntity<Object> create(@RequestBody Customer customer) {
-      return customerService.insert(customer);
-   }
+    @PutMapping(path = "/customer/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody Customer customer) {
+        return customerService.update(id, customer);
+    }
 
-   @PutMapping(path = "/customer/{id}")
-   public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody Customer customer) {
-      return customerService.update(id, customer);
-   }
-
-   @DeleteMapping(path = "/customer/{id}")
-   public ResponseEntity<Object> delete(@PathVariable Integer id) {
-      return customerService.delete(id);
-   }
+    @DeleteMapping(path = "/customer/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Integer id) {
+        return customerService.delete(id);
+    }
 }
